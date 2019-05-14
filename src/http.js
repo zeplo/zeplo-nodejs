59b1a0d4ab191669fc1d4cf4ce13d9ae4afc6e8f
Calum Moore <calum.moore@gmail.com>
2019-05-14 21:36:46 +0100

    Add methods to zeplo.http`

--- src/http.js
+++ src/http.js
@@ -1,7 +1,10 @@
-import micro from 'micro'
-
-export {
+import micro, {
   json, text, send, buffer,
 } from 'micro'
+
+micro.json = json
+micro.text = text
+micro.send = send
+micro.buffer = buffer
 
 export default micro
